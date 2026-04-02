60a31419689500f60c964e2cc9249e9a2bedf560
bobomobo1 <ethanaikman1@hotmail.com>
2026-04-02 15:58:03 -0400

    Dont start a new WTDG timer

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -110,7 +110,7 @@ int main(void)
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
   MX_USART2_UART_Init();
-  MX_IWDG_Init();
+  //MX_IWDG_Init();
   MX_USART1_UART_Init();
   /* USER CODE BEGIN 2 */
   uint32_t flag = *(uint32_t*)FLASH_FLAG_START;
